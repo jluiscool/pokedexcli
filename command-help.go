d73c757fd3a5bebef14307bcb21076a0db4521e2
Jose A <jfacademusic@gmail.com>
2023-11-25 15:50:16 -0500

    added comments

--- command-help.go
+++ command-help.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func commandHelp() error {
+	//adds a new line
 	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
+	//prints commands and their description
 	for _, cmd := range getCommands() {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
--- repl.go
+++ repl.go
@@ -8,25 +8,37 @@ import (
 )
 
 func startRepl() {
+	//initiate a reader
 	reader := bufio.NewScanner(os.Stdin)
+	//welcome message
+	fmt.Print("Welcome to the Pokedex, type help to get started: \n")
 	for {
 		fmt.Print("Pokedex > ")
+		// gets user input
 		reader.Scan()
 
+		//filter words to get a list of words typed
 		words := cleanInput(reader.Text())
+		//if nothing was typed, keep the loop going.
 		if len(words) == 0 {
 			continue
 		}
 
+		//command looks for the first word of the list
 		commandName := words[0]
 
+		//checks to see if command exists from getCommands func, uses commandName as the key
 		command, exists := getCommands()[commandName]
+		//if it exists
 		if exists {
+			//err message is the command.Callback()'s return
 			err := command.callback()
+			//prints message
 			if err != nil {
 				fmt.Println(err)
 			}
 			continue
+			//if it doesn't exists
 		} else {
 			fmt.Println("Unknown command")
 			continue
@@ -34,8 +46,11 @@ func startRepl() {
 	}
 }
 
+// cleans input from CLI
 func cleanInput(text string) []string {
+	//make words to lowercase
 	output := strings.ToLower(text)
+	// splits the lowercase string into a slice of words separated by space
 	words := strings.Fields(output)
 	return words
 }
@@ -46,7 +61,9 @@ type cliCommand struct {
 	callback    func() error
 }
 
+// list of commands
 func getCommands() map[string]cliCommand {
+	//returns a map with a key string, and a value that is cliCommand structured
 	return map[string]cliCommand{
 		"help": {
 			name:        "help",

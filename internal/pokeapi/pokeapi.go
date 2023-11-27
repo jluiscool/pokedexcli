44be47c5e61938ed5b1174fcef64fba859835dbd
Jose A <jfacademusic@gmail.com>
2023-11-26 19:30:45 -0500

    added cache field to the Client structure

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -3,19 +3,23 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	pokeacache "github.com/jluiscool/pokedexcli/internal/pokecache"
 )
 
 // string from pokeapi url
 const baseURL = "https://pokeapi.co/api/v2"
 
 type Client struct {
+	cache      pokeacache.Cache
 	httpClient http.Client
 }
 
 // always a good idea to make a function for the http client, kind of like a constructor in an object oriented language
 // gives the client a timeout, the timeout is set to one minute
-func NewClient() Client {
+func NewClient(cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokeacache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: time.Minute,
 		},

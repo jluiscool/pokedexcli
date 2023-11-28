ce7de48da352364b77ee86dffe46ef57589a3ca3
Jose A <jfacademusic@gmail.com>
2023-11-28 15:50:48 -0500

    locking for concurrency added

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,9 +1,13 @@
 package pokeacache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
 	cache map[string]cacheEntry
+	mux   *sync.Mutex
 }
 
 // a cache entry is an http body, which is stored as bytes
@@ -18,6 +22,7 @@ type cacheEntry struct {
 func NewCache(interval time.Duration) Cache {
 	c := Cache{
 		cache: make(map[string]cacheEntry),
+		mux:   &sync.Mutex{},
 	}
 
 	// reapLoop needs to run in a go routine, else it runs forever, making the tests get stuck
@@ -29,6 +34,8 @@ func NewCache(interval time.Duration) Cache {
 // adds new entries to cache, like a constructor
 // note that it uses a pointer to it directly modifies the Cache receiver
 func (c *Cache) Add(key string, val []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	c.cache[key] = cacheEntry{
 		val:       val,
 		createdAt: time.Now().UTC(),
@@ -38,10 +45,13 @@ func (c *Cache) Add(key string, val []byte) {
 // get function
 // bool tells whether that key exists or not
 func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	cacheE, ok := c.cache[key]
 	return cacheE.val, ok
 }
 
+// dont block the loop
 func (c *Cache) reapLoop(interval time.Duration) {
 	ticker := time.NewTicker(interval)
 	//ticker.C is the channel the ticker operates on
@@ -51,8 +61,11 @@ func (c *Cache) reapLoop(interval time.Duration) {
 	}
 }
 
+// block the function that the loop runs
 // purge cache after a certain interval
 func (c *Cache) reap(interval time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	timeAgo := time.Now().UTC().Add(-interval)
 	for k, v := range c.cache {
 		if v.createdAt.Before(timeAgo) {
